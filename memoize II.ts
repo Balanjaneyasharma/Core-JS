a8dc953b1e4a0d707c47453501bdd06e8304f535
Balu Sharma <balanjaneyasarma@gmail.com>
2026-01-05 23:29:45 +0530

    Add memoization function to cache function results
    Implement a memoization function that caches results based on input arguments.

---
+++ memoize II.ts
@@ -0,0 +1,63 @@
+/*
+Given a function fn, return a memoized version of that function.
+
+A memoized function is a function that will never be called twice with the same inputs. Instead it will return a cached value.
+
+fn can be any function and there are no constraints on what type of values it accepts. Inputs are considered identical if they are === to each other.
+
+Example 1:
+
+Input: 
+getInputs = () => [[2,2],[2,2],[1,2]]
+fn = function (a, b) { return a + b; }
+Output: [{"val":4,"calls":1},{"val":4,"calls":1},{"val":3,"calls":2}]
+Explanation:
+const inputs = getInputs();
+const memoized = memoize(fn);
+for (const arr of inputs) {
+  memoized(...arr);
+}
+
+For the inputs of (2, 2): 2 + 2 = 4, and it required a call to fn().
+For the inputs of (2, 2): 2 + 2 = 4, but those inputs were seen before so no call to fn() was required.
+For the inputs of (1, 2): 1 + 2 = 3, and it required another call to fn() for a total of 2.
+Example 2:
+
+Input: 
+getInputs = () => [[{},{}],[{},{}],[{},{}]] 
+fn = function (a, b) { return ({...a, ...b}); }
+Output: [{"val":{},"calls":1},{"val":{},"calls":2},{"val":{},"calls":3}]
+Explanation:
+Merging two empty objects will always result in an empty object. It may seem like there should only be 1 call to fn() because of cache-hits, however none of those objects are === to each other.
+*/
+
+type Fn = (...params: any) => any
+const VALUE = Symbol("value");
+function memoize(fn: Fn): Fn {
+    const cache = new Map();
+    return function(...args) {
+        let node = cache;
+        for(const key of args) {
+            if(!node.has(key)) {
+                node.set(key, new Map());
+            }
+            node = node.get(key);
+        }
+        if(node.has(VALUE)) return node.get(VALUE);
+        const result  = fn(...args);
+        node.set(VALUE, result);
+        return result;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
