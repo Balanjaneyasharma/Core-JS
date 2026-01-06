11db612a10b88dbd2d8e5c08e787efd99e991610
Balu Sharma <balanjaneyasarma@gmail.com>
2026-01-07 00:26:38 +0530

    Add callPolyfill method to Function prototype
    Implemented callPolyfill method for function context binding.

---
+++ call-polyfill.ts
@@ -0,0 +1,33 @@
+/*
+Enhance all functions to have the callPolyfill method. The method accepts an object obj as its first parameter and any number of additional arguments. The obj becomes the this context for the function. The additional arguments are passed to the function (that the callPolyfill method belongs on).
+
+For example if you had the function:
+
+function tax(price, taxRate) {
+  const totalCost = price * (1 + taxRate);
+  console.log(`The cost of ${this.item} is ${totalCost}`);
+}
+Calling this function like tax(10, 0.1) will log "The cost of undefined is 11". This is because the this context was not defined.
+
+However, calling the function like tax.callPolyfill({item: "salad"}, 10, 0.1) will log "The cost of salad is 11". The this context was appropriately set, and the function logged an appropriate output.
+
+Please solve this without using the built-in Function.call method.
+*/
+type JSONValue = null | boolean | number | string | JSONValue[] | { [key: string]: JSONValue };
+
+interface Function {
+    callPolyfill(context: Record<string, JSONValue>, ...args: JSONValue[]): JSONValue;
+}
+
+Function.prototype.callPolyfill = function(context, ...args): JSONValue {
+    const ctx = (context || globalThis) as Record<PropertyKey, any> ;
+    const tempSymbol = Symbol();
+    ctx[tempSymbol] = this;
+    const result = ctx[tempSymbol](...args);
+    delete ctx[tempSymbol];
+    return result;
+}
+/**
+ * function increment() { this.count++; return this.count; }
+ * increment.callPolyfill({count: 1}); // 2
+ */
